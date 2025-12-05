593068a1c6855b1e7a8782d390162d840ae2e89c
kavinduAshen <kavinduashensenarath@gmail.com>
2025-12-06 02:07:24 +0530

    feat: add profile picture and animation in hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,11 +1,28 @@
+import { motion } from "framer-motion";
+
 function Hero() {
-
-
     return (
-        <section id="hero" className="h-screen flex items-center justify-center bg-[#151925]">
-            <h1 className="text-5xl font-bold text-blue-50">Hi, I'm Ashen</h1>
+        <section
+            id="hero"
+            className="h-screen flex items-center justify-center bg-[#151925]"
+        >
+            <motion.div
+                initial={{ scale: 0, rotate: -30, opacity: 0 }}
+                animate={{ scale: 1, rotate: 0, opacity: 1 }}
+                transition={{ duration: 1.2, ease: "easeOut" }}
+                className="absolute w-60 h-30 bg-indigo-600 rounded-full rotate-20 z-0 mt-20 mb-50"
+            ></motion.div>
+
+            <img
+                src="profic-m.png"
+                alt="Profile"
+                className="w-64 z-10 absolute pb-50"
+            />
+            <div className='pd-30'>
+                hi ia a m
+            </div>
         </section>
-    )
+    );
 }
 
 export default Hero;
