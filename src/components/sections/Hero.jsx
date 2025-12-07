2ddcdc86fa8a043beb4c4d49d8dc25f6b26a9a62
kavinduAshen <kavinduashensenarath@gmail.com>
2025-12-08 00:47:42 +0530

    feat: add hero section with Knight Rider typing animation

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,6 +1,37 @@
 import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 function Hero() {
+    const texts = [
+        "Front-End Developer",
+        "UI/UX Developer",
+        "Back-End Developer",
+    ];
+    const [text, setText] = useState("");
+    const [index, setIndex] = useState(0);
+    const [subIndex, setSubIndex] = useState(0);
+    const [deleting, setDeleting] = useState(false);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (!deleting && subIndex === texts[index].length) {
+                setTimeout(() => setDeleting(true), 700);
+                return;
+            }
+
+            if (deleting && subIndex === 0) {
+                setDeleting(false);
+                setIndex((prev) => (prev + 1) % texts.length);
+                return;
+            }
+
+            setSubIndex((prev) => prev + (deleting ? -1 : 1));
+            setText(texts[index].substring(0, subIndex));
+        }, deleting ? 50 : 100);
+
+        return () => clearTimeout(timeout);
+    }, [subIndex, index, deleting]);
+
     return (
         <section
             id="hero"
@@ -18,8 +49,22 @@ function Hero() {
                 alt="Profile"
                 className="w-64 z-10 absolute pb-50"
             />
-            <div className='pd-30'>
-                hi ia a m
+            <div className="text-center z-10 mt-80 text-white">
+                <p className='text-sm mb-2'>Hello it's Me</p>
+
+                <h1 className="text-3xl font-bold mb-1">
+                    Ashen senarath </h1>
+                <h2 className="text-lg mb-4" > And I'm a {" "} <span className="text-indigo-400 font-semibold">{text}</span> <span className="ml-1 animate-pulse">|</span></h2>
+                <p className="text-gray-400 max-w-md mb-6">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                    className="px-6 py-2 border border-indigo-500 rounded-full text-white hover:bg-indigo-600 hover:text-white transition">
+                    Download CV
+                </motion.button>
             </div>
         </section>
     );
